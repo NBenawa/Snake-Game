97d1657eb2e06b7e1a9330a76f5f798b49aebd18
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-15 10:52:35 +0200

    creating simple snake

--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ import turtle
 # dimensions
 WIDTH = 500
 HEIGHT = 500
+DELAY = 400
 
 # screen 
 screen = turtle.Screen()
@@ -10,10 +11,19 @@ screen.setup(WIDTH, HEIGHT)
 screen.title("Snake Game")
 screen.bgcolor("cyan")
 
+# stamper
+stamper = turtle.Turtle()
+stamper.shape("circle")
+stamper.color("red")
+stamper.penup()
+
 # snake
-snake = turtle.Turtle()
-snake.shape("circle")
-snake.color("red")
+snake = [[0, 0], [20, 0], [40, 0], [60, 0]]
+
+# draw snake
+for segment in snake:
+    stamper.goto(segment[0], segment[1])
+    stamper.stamp()
 
 # finish game
 turtle.done()
