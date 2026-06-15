40104a8e7e705ec533d33c2faa34b5b9749ab7cf
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-15 12:59:38 +0200

    controlling the snake direction

--- snake.py
+++ snake.py
@@ -5,13 +5,42 @@ WIDTH = 500
 HEIGHT = 500
 DELAY = 400
 
+offsets = {
+    "up": (0, 20),
+    "down": (0, -20),
+    "left": (-20, 0),
+    "right": (20, 0)
+}
+
+# movements
+def go_up():
+    global snake_direction
+    if snake_direction != "down":
+        snake_direction = "up"
+
+def go_down():
+    global snake_direction
+    if snake_direction != "up":
+        snake_direction = "down"
+
+def go_right():
+    global snake_direction
+    if snake_direction != "left":
+        snake_direction = "right"
+
+def go_left():
+    global snake_direction
+    if snake_direction != "right":
+        snake_direction = "left"
+
 # move snake
 def move():
     # remove existing stamps
     stamper.clearstamps()
 
     new_head = snake[-1].copy()
-    new_head[0] += 20
+    new_head[0] += offsets[snake_direction][0]
+    new_head[1] += offsets[snake_direction][1]
 
     snake.append(new_head)
     snake.pop(0)
@@ -31,6 +60,13 @@ screen.setup(WIDTH, HEIGHT)
 screen.title("Snake Game")
 screen.bgcolor("cyan")
 
+# key controlling
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+screen.onkey(go_right, "Right")
+screen.onkey(go_left, "Left")
+
 # stamper
 stamper = turtle.Turtle()
 stamper.shape("circle")
@@ -39,6 +75,7 @@ stamper.penup()
 
 # snake
 snake = [[0, 0], [20, 0], [40, 0], [60, 0]]
+snake_direction = "up"
 
 # draw snake
 for segment in snake:
